4224d50f4c2f525c88d8ac2c2dbd030168414c94
Sagar <sssapre28@gmail.com>
2023-09-19 11:43:16 +0530

    selenium_first_program

---
+++ Selenium_first_program.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+driver = webdriver.Chrome()
+driver.get("https://www.sbimf.com/portfolios")
+
+driver.implicitly_wait(10)
+
+##click on dropdown to make it visible
+dropd = driver.find_element(By.XPATH,'//*[@id="PSYear"]').click()
+time.sleep(1)
+##click on 2023
+dropd = driver.find_element(By.XPATH,'//*[@id="PSYear"]/div[2]/div[2]').click()
+##for 2022
+#//*[@id="PSYear"]/div[2]/div[3]
+
+time.sleep(1)
+
+selectdnld=driver.find_element(By.XPATH,'//*[@id="tblPortfoliosheets"]/tr[1]/td[1]/a').click()
+
+time.sleep(3)
+
+driver.close()
