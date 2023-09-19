64052afc3f5a7a30b725887b2e70744a53cb57f9
Sagar <sssapre28@gmail.com>
2023-09-19 17:20:52 +0530

    selenium final

--- Selenium_first_program.py
+++ Selenium_first_program.py
@@ -10,13 +10,13 @@ driver.implicitly_wait(10)
 
 ##click on dropdown to make it visible
 dropd = driver.find_element(By.XPATH,'//*[@id="PSYear"]').click()
-time.sleep(1)
+time.sleep(3)
 ##click on 2023
 dropd = driver.find_element(By.XPATH,'//*[@id="PSYear"]/div[2]/div[2]').click()
 ##for 2022 xpath is as follows
 #//*[@id="PSYear"]/div[2]/div[3]
 
-time.sleep(1)
+time.sleep(3)
 
 selectdnld=driver.find_element(By.XPATH,'//*[@id="tblPortfoliosheets"]/tr[1]/td[1]/a').click()
 
