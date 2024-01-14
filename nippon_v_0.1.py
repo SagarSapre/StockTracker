f97638b22a7268ed58313a6dab51ec80ea18f59f
Sagar <sssapre28@gmail.com>
2024-01-14 16:56:25 +0530

    icici + nippon

--- nippon_v_0.1.py
+++ nippon_v_0.1.py
@@ -1,10 +1,26 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
+import requests
 from requests import get
 import json
+from bs4 import BeautifulSoup
 
-driver = webdriver.Chrome()
-driver.get("https://mf.nipponindiaim.com/investor-service/downloads/factsheet-and-other-portfolio-disclosures")
+r=requests.get("https://mf.nipponindiaim.com/investor-service/downloads/factsheet-and-other-portfolio-disclosures")
+baseurl="https://mf.nipponindiaim.com"
+htmlcontent=r.content
+soup=BeautifulSoup(htmlcontent,'html.parser')
+scrape = soup.find_all('div', class_="contentWrap")
 
-driver.close()
+names=[]
+url=[]
+for item in scrape:
+    a=item.find_all('a')
+for item in a:
+    link=baseurl+item['href']
+    url.append(link)
+    name=link.split('/')[-1]
+    names.append(name)
+
+z=dict(zip(names,url))
+with open("nippon.json", "w") as outfile:
+    json.dump(z,outfile)    
+
+
